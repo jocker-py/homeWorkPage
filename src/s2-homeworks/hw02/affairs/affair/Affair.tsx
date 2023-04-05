0e9beb013059390f0671e31722a53a85cad1af0e
jocker-py <buladzislau@gmail.com>
2023-04-05 15:13:54 +0300

    feat: add onClick behaviour for button

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -35,8 +35,7 @@ function Affair(props: AffairPropsType) {
       <button
         id={"hw2-button-delete-" + props.affair._id}
         className={buttonClass}
-        // need to fix
-
+        onClick={deleteCallback}
       >
         {/*текст кнопки могут изменить студенты*/}
         X
