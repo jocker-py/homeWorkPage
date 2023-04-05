39369c4cccb7567ed2c0763b6284b238eac9d315
jocker-py <buladzislau@gmail.com>
2023-04-05 15:27:19 +0300

    refactor: remove comments

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -17,16 +17,16 @@ import s2 from "../../s1-main/App.module.css";
 * */
 
 // types
-export type AffairPriorityType = "high" | "middle" | "low";  // need to fix any
+export type AffairPriorityType = "high" | "middle" | "low";
 export type AffairType = {
-  _id: number // need to fix any
-  name: string // need to fix any
+  _id: number 
+  name: string 
   priority: AffairPriorityType
 }
 export type FilterType = "all" | AffairPriorityType
 
 // constants
-const defaultAffairs: Array<AffairType> = [ // need to fix any
+const defaultAffairs: Array<AffairType> = [ 
   {_id: 1, name: "React", priority: "high"}, // студенты могут изменить содержимое name и количество элементов в массиве, ...priority не менять!
   {_id: 2, name: "anime", priority: "low"},
   {_id: 3, name: "games", priority: "low"},
@@ -35,23 +35,23 @@ const defaultAffairs: Array<AffairType> = [ // need to fix any
 ];
 
 // pure helper functions
-export const filterAffairs = (affairs: Array<AffairType>, filter: FilterType): Array<AffairType> => { // need to fix any
+export const filterAffairs = (affairs: Array<AffairType>, filter: FilterType): Array<AffairType> => { 
 
 
-  return affairs; // need to fix
+  return affairs; 
 };
-export const deleteAffair = (affairs: Array<AffairType>, _id: number): any => { // need to fix any
+export const deleteAffair = (affairs: Array<AffairType>, _id: number): any => { 
 
-  return affairs; // need to fix
+  return affairs; 
 };
 
 function HW2() {
-  const [affairs, setAffairs] = useState<Array<AffairType>>(defaultAffairs); // need to fix any
+  const [affairs, setAffairs] = useState<Array<AffairType>>(defaultAffairs); 
   const [filter, setFilter] = useState<FilterType>("all");
 
   const filteredAffairs = filterAffairs(affairs, filter);
-  const deleteAffairCallback = (_id: number) => { // need to fix any
-    // need to fix
+  const deleteAffairCallback = (_id: number) => { 
+    
   };
 
   return (
