ffac8b4e54d318e21f0abd3b5be963a2cdab756c
jocker-py <buladzislau@gmail.com>
2023-04-11 18:59:01 +0300

    feat: add types for test cases

--- src/s2-homeworks/hw03/tests/pureAddUserCallback.test.tsx
+++ src/s2-homeworks/hw03/tests/pureAddUserCallback.test.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import {pureAddUserCallback} from '../HW3'
+import React from "react";
+import {pureAddUserCallback, UsersType} from "../HW3";
 
-let initialState: any[]
-const setName = (a: any[]) => {
-    initialState = a
-}
+let initialState: UsersType;
+const setName = (a: UsersType) => {
+  initialState = a;
+};
 
 beforeEach(() => {
-    initialState = []
-})
+  initialState = [];
+});
 
-test('name 1', () => {
-    pureAddUserCallback('name', setName, initialState)
-    expect(initialState.length).toBe(1)
-    expect(initialState[0].name).toBe('name')
-    expect(!!initialState[0]._id).toBe(true)
-})
+test("name 1", () => {
+  pureAddUserCallback("name", setName, initialState);
+  expect(initialState.length).toBe(1);
+  expect(initialState[0].name).toBe("name");
+  expect(!!initialState[0]._id).toBe(true);
+});
