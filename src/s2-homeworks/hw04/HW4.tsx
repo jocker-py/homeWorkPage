ebe874cae9e8106de873772cf5ea34581fadbdee
jocker-py <buladzislau@gmail.com>
2023-04-16 18:22:47 +0300

    feat: add hr

--- src/s2-homeworks/hw04/HW4.tsx
+++ src/s2-homeworks/hw04/HW4.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import s2 from '../../s1-main/App.module.css'
-import Stand from './Stand'
+import React from "react";
+import s2 from "../../s1-main/App.module.css";
+import Stand from "./Stand";
 
 /*
 * 1 - понять (и простить) SuperInputText
@@ -10,15 +10,18 @@ import Stand from './Stand'
 * */
 
 const HW4 = () => {
-    return (
-        <div id={'hw4'}>
-            <div className={s2.hwTitle}>Homework #4</div>
-            {/*демонстрация возможностей компонент:*/}
-            <div className={s2.hw}>
-                <Stand />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div id={"hw4"}>
+      <hr/>
+      <div className={s2.hwTitle}>Homework #4</div>
+      {/*демонстрация возможностей компонент:*/}
+      <hr/>
+      <div className={s2.hw}>
+        <Stand/>
+      </div>
+      <hr/>
+    </div>
+  );
+};
 
-export default HW4
+export default HW4;
