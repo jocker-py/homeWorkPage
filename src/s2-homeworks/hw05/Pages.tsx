0f09f3f72b681d641de61742d2410c26c4716119
jocker-py <buladzislau@gmail.com>
2023-04-25 17:39:16 +0300

    refactor: simplify code

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -6,11 +6,9 @@ import Junior from "./pages/Junior";
 import JuniorPlus from "./pages/JuniorPlus";
 
 export const PATH = {
-  ROOT: "/",
   PRE_JUNIOR: "/pre-junior",
   JUNIOR: "/junior",
   JUNIOR_PLUS: "/junior-plus",
-  ERROR404: "/*",
 };
 
 function Pages() {
@@ -20,14 +18,14 @@ function Pages() {
       <Routes>
         {/*роутинг будут писать студенты*/}
         {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-        <Route path={PATH.ROOT} element={<PreJunior/>}/>
+        <Route path={"/"} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
         {/*роуты для /pre-junior, /junior, /junior-plus*/}
         <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
         <Route path={PATH.JUNIOR} element={<Junior/>}/>
         <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
 
         {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-        <Route path={PATH.ERROR404} element={<Error404/>}/>
+        <Route path={"/*"} element={<Error404/>}/>
       </Routes>
     </div>
   );
