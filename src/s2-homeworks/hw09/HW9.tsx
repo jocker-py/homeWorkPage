cb2b1d57de734a962e50e1bf9db7d083ac7bc3f0
jocker-py <buladzislau@gmail.com>
2023-05-21 12:48:07 +0300

    feat: add hr

--- src/s2-homeworks/hw09/HW9.tsx
+++ src/s2-homeworks/hw09/HW9.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import Clock from './Clock'
-import s2 from '../../s1-main/App.module.css'
+import React from "react";
+import Clock from "./Clock";
+import s2 from "../../s1-main/App.module.css";
 
 /*
 * 1 - в файле Clock.tsx дописать функции stop, start, onMouseEnter, onMouseLeave
@@ -10,15 +10,16 @@ import s2 from '../../s1-main/App.module.css'
 * */
 
 const HW9 = () => {
-    return (
-        <div id={'hw9'}>
-            <div className={s2.hwTitle}>Homework #9</div>
+  return (
+    <div id={"hw9"}>
+      <div className={s2.hwTitle}>Homework #9</div>
+      <hr/>
+      <div className={s2.hw}>
+        <Clock/>
+      </div>
+      <hr/>
+    </div>
+  );
+};
 
-            <div className={s2.hw}>
-                <Clock />
-            </div>
-        </div>
-    )
-}
-
-export default HW9
+export default HW9;
