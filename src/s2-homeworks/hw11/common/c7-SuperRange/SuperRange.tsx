9fdc58b9082b9bcd776435208daadd18524cf16c
jocker-py <buladzislau@gmail.com>
2023-06-11 14:29:06 +0300

    feat: add solution for HW11

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,15 +1,35 @@
-import React from 'react'
-import {Slider, SliderProps} from '@mui/material'
+import React from "react";
+import {Slider, SliderProps, styled} from "@mui/material";
 
 const SuperRange: React.FC<SliderProps> = (props) => {
-    return (
-        <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
-        />
-    )
-}
+  return <PrettoSlider{...props} />;
+};
 
-export default SuperRange
+export default SuperRange;
+
+const PrettoSlider = styled(Slider)({
+  width: "60%",
+  color: "#52af77",
+  height: 4,
+  "& .MuiSlider-track": {
+    border: "none",
+  },
+  "& .MuiSlider-thumb": {
+    height: 18,
+    width: 18,
+    backgroundColor: "#fff",
+    border: "2px solid currentColor",
+    "&:focus, &:hover, &.Mui-active, &.Mui-focusVisible": {
+      boxShadow: "inherit",
+    },
+    "&:before": {
+      width: 6,
+      height: 6,
+      borderRadius: "50%",
+      boxShadow: "inset 10px 10px #52af77",
+    },
+  },
+  "& .MuiSlider-rail": {
+    backgroundColor: "#8B8B8B",
+  },
+});
